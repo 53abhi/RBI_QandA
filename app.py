66fcf54039741f9da2a5a4d7625f5ea1c2780827
53abhi <115625558+53abhi@users.noreply.github.com>
2025-03-13 11:58:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-
+#Changes made
 # Load environment variables
 load_dotenv()
 API_KEY = os.getenv("MISTRAL_API_KEY", "K5QN2Uig3Xd1ihGAVkCyrGN5MIWyBHjp")
